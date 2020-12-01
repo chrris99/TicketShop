838079976fa48ea4f5754b042ebff364f799b3be
Christian Conforti <christian.conforti@dxc.com>
2020-12-01 12:59:21 +0100

    skaffold issues fixed

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,10 +4,6 @@ import { json } from 'body-parser'
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi theree')
-})
-
 app.listen(3000, () => {
-    console.log('Auth listening on port 3000')
+    console.log('Listening on port 3000')
 })
