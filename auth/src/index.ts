97f750a0dfebb7f5c62424b4fe478ba4f3f9c761
chrris99 <conforti.christian@gmail.com>
2020-12-02 12:58:57 +0100

    added ssl certificate

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -19,11 +19,6 @@ app.use(signInRouter)
 app.use(signOutRouter)
 app.use(signUpRouter)
 
-app.get('/auth', (req, res)  => {
-    console.log(req)
-    res.status(200).send('Authentication succesful')
-})
-
 app.all('*', () => {
     throw new RouteNotFoundError()
 })
