55f8452fa49d444e3ea4275e52429990e50f9c6b
chrris99 <conforti.christian@gmail.com>
2020-12-03 11:59:53 +0100

    header and signout page created

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import buildClient from '../api/build-client'
+import Header from '../components/header'
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
-        <div className="container">
-            <h1>Header</h1>
-            <Component {...pageProps} />
+        <div>
+            <Header currentUser={currentUser} />
+            <div className="container">
+                <Component {...pageProps} />
+            </div>
         </div>
     )
 }
@@ -14,9 +17,15 @@ AppComponent.getInitialProps = async appContext => {
     const client = buildClient(appContext.ctx)
     const { data } = await client.get('/api/users/currentuser')
 
-    console.log(data)
-    
-    return data
+    let pageProps = {}
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+    }
+
+    return {
+        pageProps,
+        ...data
+    }
 }
 
 export default AppComponent
